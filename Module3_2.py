2ab4f48f9145062262623a5db1004ab90f59d82c
korbandallasjr <korbandallasjr@gmail.com>
2024-10-29 14:31:56 +0300

    Update Module3_2.py

--- Module3_2.py
+++ Module3_2.py
@@ -1,21 +1,15 @@
 def send_email(message, recipient, sender='university.help@gmail.com'):
-    splitted = recipient.split('.')
-    splitted2 = sender.split('.')
-    if sender == recipient:
+    domains_ = ('.com', '.net', '.ru')
+    if recipient.find('@') == -1 or sender.find('@') == -1 or recipient.endswith(domains_) == False or sender.endswith(
+            domains_) == False:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
+    elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
-    elif splitted[-1] != 'com' and splitted[-1] != 'ru' and splitted[-1] != 'net':
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    elif splitted2[-1] != 'com' and splitted2[-1] != 'ru' and splitted2[-1] != 'net':
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
-    else:
-        for character in recipient:
-            if character == '@':
-                for character in sender:
-                    if character == '@':
-                        if sender != '':
-                            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-                        else:
-                            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ!Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
